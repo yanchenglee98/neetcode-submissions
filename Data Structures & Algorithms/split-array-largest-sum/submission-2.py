409b1299aada19417e59632693a20fadce930184
yanchenglee98 <59784190+yanchenglee98@users.noreply.github.com>
2026-05-22 15:56:02 +0800

    Add: split-array-largest-sum - submission-2

---
+++ Data Structures & Algorithms/split-array-largest-sum/submission-2.py
@@ -0,0 +1,23 @@
+#https://neetcode.io/problems/split-array-largest-sum/solution
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        def canSplit(largest):
+            numSplit = 1
+            currSum = 0
+            for n in nums:
+                if currSum + n > largest:
+                    numSplit+=1
+                    currSum = n
+                    if numSplit > k:
+                        return False
+                else:
+                    currSum+=n
+            return True
+        l, r = max(nums), sum(nums)
+        while l <= r:
+            m = (l+r)//2
+            if canSplit(m):
+                r = m-1
+            else:
+                l = m+1
+        return l
